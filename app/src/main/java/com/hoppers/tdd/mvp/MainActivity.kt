58a9f5bbdae381657d8f3a6f62407171ccf32722
jk2pr <jk2praj@gmail.com>
2018-07-27 15:51:39 +0800

    Initial commit

---
+++ app/src/main/java/com/hoppers/tdd/mvp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.hoppers.tdd.mvp
+
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hoppers.tdd_mvp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
